4e176ddbaa8acc6e065601bdc5d961bbac6207ba
Michał Wilmowicz <wilmowicz@gmail.com>
2024-10-22 18:44:42 +0200

    Basic implementation

---
+++ src/main/java/pl/michal/pomyslownik/PomyslownikApplication.java
@@ -0,0 +1,13 @@
+package pl.michal.pomyslownik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomyslownikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PomyslownikApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/michal/pomyslownik/PomyslownikApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.michal.pomyslownik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomyslownikApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
